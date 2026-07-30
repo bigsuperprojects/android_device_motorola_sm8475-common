60e3b3b59e6928f8ebd2f5333458a239eb34ddf2
bigsupersquid <0.uradork.0@gmail.com>
2026-07-30 09:14:32 -0500

    add linux container kernel config fragment
    Change-Id: Id927007ae20a7404088acbce423eb9971fee14ba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,7 +75,8 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/waipio_GKI.config \
     vendor/ext_config/moto-waipio.config \
-    vendor/ext_config/moto-waipio-gki.config
+    vendor/ext_config/moto-waipio-gki.config \
+    vendor/ext_config/moto-waipio-linux.config
 
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/motorola/sm8475-modules
 
